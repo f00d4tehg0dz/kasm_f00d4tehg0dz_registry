4e929553d5337854c7998548c1db753cc687a714
Adrian Chrysanthou <adrianvfx@gmail.com>
2024-04-06 21:15:57 -0700

    Update next.config.js
    update config with new values

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'f00d4tehg0dz Custom Kasm Workspaces',
+    description: 'f00d4tehg0dz store of custom Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://f00d4tehg0dz.github.io/kasm_f00d4tehg0dz_registry/',
+    contactUrl: 'https://github.com/f00d4tehg0dz/kasm_f00d4tehg0dz_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
